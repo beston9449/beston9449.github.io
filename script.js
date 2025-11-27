bb69cde62e0ff5336dd405672f080b71cd211e57
Beston Mobasheri <mobasheribeston@gmail.com>
2025-11-27 10:11:33 +0200

    Fixed counters

--- script.js
+++ script.js
@@ -5,25 +5,51 @@ container.style.border = '2px solid black';
 
 let addButton = document.querySelector('.addBtn');
 let taskInput = document.querySelector('.inputTask');
+let body = document.querySelector('body');
+let counter = document.createElement('span');
+body.appendChild(counter);
 
+let activeCounter = 0;
+let completedCounter = 0;
+
+function updateCounterText() {
+  return `Active Tasks: ${activeCounter} Completed Tasks: ${completedCounter}`;
+}
 function getTask() {
   let taskBox = document.createElement('div');
+  taskBox.dataset.completed = 'false';
   let taskText = document.createElement('span');
   let deleteButton = document.createElement('button');
+  let completeButton = document.createElement('button');
 
   taskText.textContent = taskInput.value;
   taskBox.appendChild(taskText);
   taskBox.appendChild(deleteButton);
+  taskBox.appendChild(completeButton);
   container.appendChild(taskBox);
   deleteButton.textContent = 'Delete';
+  completeButton.textContent = 'Complete';
 
   deleteButton.addEventListener('click', () => {
+    if (taskBox.dataset.completed === 'true') {
+      --completedCounter;
+    } else {
+      --activeCounter;
+    }
+    counter.textContent = updateCounterText();
     taskBox.remove();
   });
 
-  taskBox.addEventListener('click', () => {
-    taskBox.style.backgroundColor = 'green';
-    taskText.style.textDecoration = 'line-through';
+  completeButton.addEventListener('click', () => {
+    // taskText.style.textDecoration = 'line-through';
+    if (taskBox.dataset.completed === 'false') {
+      taskBox.dataset.completed = 'true';
+      taskBox.style.backgroundColor = 'green';
+      completedCounter++;
+      --activeCounter;
+      counter.textContent = updateCounterText();
+      
+    }
   });
 
   taskInput.value = '';
@@ -35,6 +61,8 @@ addButton.addEventListener('click', () => {
     alert('Empty task not allowed');
   } else {
     getTask();
+    activeCounter++;
+    counter.textContent = updateCounterText();
   }
 });
 
